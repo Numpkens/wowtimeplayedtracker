e0d57360cf335aac949d9b969406d778e6664d5f
Numpkens <numpkins1228@gmail.com>
2025-02-14 14:41:50 +0800

    Modify lua file so that the frame disappears after 20 seconds

--- PlayedTimeTracker.lua
+++ PlayedTimeTracker.lua
@@ -4,22 +4,11 @@ local frame = CreateFrame("Frame")
 --Register the events
 frame:RegisterEvent("PLAYER_LOGIN")
 
---Getting the time played by the player
-local totalPlayTime, _, _=GetTimePlayed()
-
---Converting the time played into hours, minutes and seconds
-local hours = math.floor(totalPlayTime/3600)
-local minutes = math.floor((totalPlayTime%3600)/60)
-local seconds = totalPlayTime%60
-
---Format the time played
-local formattedTime = string.format("%d hours, %d minutes, %d seconds", hours, minutes, seconds)
-
---Display the time played to the user
-local frame = CreateFrame("Frame", "PlayedTimeFrame", UIParent)
-frame:SetSize(200, 50)
-frame:SetPoint("CENTER", UIParent, Center)
-frame:SetBackdrop({
+--Create the display frame
+local displayFrame = CreateFrame("Frame", "PlayedTimeFrame", UIParent, "BackdropTemplate")
+displayFrame:SetSize(800, 50)
+displayFrame:SetPoint("CENTER", UIParent, "CENTER")
+displayFrame:SetBackdrop({
     bgFile = "Interface/Tooltips/UI-Tooltip-Background",
     edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
     tile = true,
@@ -32,18 +21,30 @@ frame:SetBackdrop({
         bottom = 4
     }
 })
-frame.text = frame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-frame.text:SetPoint("CENTER", frame, "CENTER")
-frame.text:SetText("Total time played: "..formattedTime)
+displayFrame.text = displayFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+displayFrame.text:SetPoint("CENTER", displayFrame, "CENTER")
+displayFrame.text:SetFont("fonts/morpheus.ttf", 28, "OUTLINE")
 
-frame:RegisterEvent("PLAYER_LOGIN")
-frame:SetScript("OnEvent", function(self, event, argi)
+-- Initially hide the display frame
+displayFrame:Hide()
+
+--Event handler function
+frame:SetScript("OnEvent", function(self, event, arg1)
     if event == "PLAYER_LOGIN" then
-        local totalPlayTime, _, _=GetTimePlayed()
-        local hours = math.floor(totalPlayTime/3600)
-        local minutes = math.floor((totalPlayTime%3600)/60)
-        local seconds = totalPlayTime%60
+        RequestTimePlayed()
+    elseif event == "TIME_PLAYED_MSG" then
+        local totalPlayTime = arg1
+        local hours = math.floor(totalPlayTime / 3600)
+        local minutes = math.floor((totalPlayTime % 3600) / 60)
+        local seconds = totalPlayTime % 60
         local formattedTime = string.format("%d hours, %d minutes, %d seconds", hours, minutes, seconds)
-        text:SetText(formattedTime)
+        displayFrame.text:SetText("Total time played: " .. formattedTime)
+        displayFrame:Show()
+        C_Timer.After(20, function()
+            displayFrame:Hide()
+        end)
     end
 end)
+
+--Register the TIME_PLAYED_MSG event to get the playtime
+frame:RegisterEvent("TIME_PLAYED_MSG")
